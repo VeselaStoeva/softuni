2112077865b9c9cf41fbb057a25131605e3175d9
Vesela <vesela@abv.bg>
2023-01-02 11:49:42 -0800

    initial commit

---
+++ Conditional_Statements_Lab/excellent_result.cs
@@ -0,0 +1,15 @@
+using System;
+namespace task14
+{
+    class Program
+    {
+        static void Main()
+        {
+            double grade=double.Parse(Console.ReadLine());
+            if(grade>=5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
